d7ea1f3c036e21f0d7def3a9f549572ea1c1831f
naganandana <naganandana17@gmail.com>
2024-04-07 19:02:27 +0400

    commit

---
+++ Sorting Algorithms/BubbleSort.java
@@ -0,0 +1,26 @@
+public class BubbleSort{
+
+    public static void main(String[] args){
+
+        int array[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+        bubbleSort(array);
+
+        for(int i : array){
+            System.out.println(i);
+        }
+    }
+
+    public static void bubbleSort(int array[]){
+
+        for(int i = 0; i < array.length - 1; i++){
+            for(int j = 0; j < array.length - i - 1; j++){
+                if (array[j] > array[j + 1]){ // for descending order, just change > to < 
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
